386ed3ba68472f8f973999689b5382e34c8cf68d
Fatos <fatos.shala@protonmail.ch>
2020-05-12 00:42:56 +0200

    finish code start setup script

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+from distutils.core import setup
+
+setup(
+    name="Distutils",
+    version="1.4",
+    description="Python Distribution Utilities",
+    author="Fatos Shala",
+    author_email="fatos.shala@protonmail.ch",
+    url="https://github.com/Faetu/Mirror-Manager",
+    packages=["distutils", "distutils.command"],
+)
+
